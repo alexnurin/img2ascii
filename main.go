d4b3d7f20e7607d242a901f83277cd2a3dedd07b
alexnurin <an362@mail.ru>
2019-12-28 19:42:01 +0300

    Can set custom 'Szhatie-coefficient'

--- main.go
+++ main.go
@@ -27,24 +27,34 @@ func decodeImageFile(imgName string) (image.Image, error) {
 	return img, err
 }
 
-func processCell(c color.Color) rune {
+func getGray(c color.Color) int {
 	gc := color.GrayModel.Convert(c)
 	r, _, _, _ := gc.RGBA()
 	r = r >> 8
+	return int(r)
+}
+
+func processPixel(r int) rune {
+	//r := getGray(c)
 	chars := []rune("@#%*+-:. ")
-	id := int(r) * len(chars) / 256
+	id := r * len(chars) / 256
 	return chars[id]
 }
 
-func getCell(img image.Image, y int, x int) rune {
-	res :=  processCell(img.At(x, y))
-	return res
+func processCell(img image.Image, y int, x int, sz_x int, sz_y int) rune {
+	count := 0
+	for i := x; i < x+sz_x; i++ {
+		for j := y; j < y+sz_y; j++ {
+			count += getGray(img.At(i, j))
+		}
+	}
+	res := count / (sz_x * sz_y)
+	return processPixel(res)
 }
 
 func convertToAscii(img image.Image) [][]rune {
 	sz_x := img.Bounds().Dx()
 	sz_y := img.Bounds().Dy()
-
 
 	if sz_x > *img_size_x && *img_size_x > 0 {
 		sz_x = *img_size_x
@@ -53,16 +63,21 @@ func convertToAscii(img image.Image) [][]rune {
 		sz_y = *img_size_y
 	}
 
-	textImg := make([][]rune, sz_y)
-	fmt.Println(sz_x, sz_y)
+	delta_x := 10
+	delta_y := 15
+	sz_y_new := sz_y/delta_y + 1
+	sz_x_new := sz_x/delta_x + 1
 
-	for i := 0; i < sz_y; i++ {
-		textImg[i] = make([]rune, sz_x)
+	textImg := make([][]rune, sz_y_new)
+	for i := 0; i < sz_y_new; i++ {
+		textImg[i] = make([]rune, sz_x_new)
 	}
 
-	for i := 0; i < sz_y; i++ {
-		for j := 0; j < sz_x; j++ {
-			textImg[i][j] = getCell(img, i, j)
+	//fmt.Println(sz_x, sz_y, sz_x_new, sz_y_new)
+
+	for i := 0; i < sz_y; i += delta_y {
+		for j := 0; j < sz_x; j += delta_x {
+			textImg[i/delta_y][j/delta_x] = processCell(img, i, j, delta_x, delta_y)
 		}
 	}
 	//for i := range textImg {
@@ -89,10 +104,12 @@ func main() {
 	}
 
 	textImg := convertToAscii(img_decoded)
+	//fmt.Println(textImg)
 	for i := range textImg {
 		for j := range textImg[i] {
 			fmt.Printf("%c", textImg[i][j])
 		}
 		fmt.Println()
 	}
+
 }
