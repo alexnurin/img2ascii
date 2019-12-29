b79774e3b0728444205f862676e5da0e0b3da406
Нарин Алексей <an362@mail.ru>
2019-12-29 13:59:49 +0300

    Base color version

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ var (
 	img_size_x = flag.Int("w", 0, "Image width")
 	quadro     = flag.Bool("q", false, "Convert to quadro size")
 	normalize  = flag.Bool("n", false, "Convert to normal size (3:2)")
+	colors     = []string{"\033[0;31m%c\033[0m", "\033[0;32m%c\033[0m", "\033[0;34m%c\033[0m"}
 )
 
 func decodeImageFile(imgName string) (image.Image, error) {
@@ -36,13 +37,18 @@ func getGray(c color.Color) int {
 	return int(r)
 }
 
+func getColor(c color.Color) (int, int, int) {
+	r, g, b, _ := c.RGBA()
+	return int(r), int(g), int(b)
+}
+
 func getChar(r int) rune {
 	chars := []rune("@#%*+-:. ")
 	id := r * len(chars) / 256
 	return chars[id]
 }
 
-func processCell(img image.Image, y int, x int, sz_x int, sz_y int) rune {
+func processCell(img image.Image, y int, x int, sz_x int, sz_y int) (rune, int) {
 	count := 0
 	n, _ := minMax(x+sz_x, img.Bounds().Dx())
 	m, _ := minMax(y+sz_y, img.Bounds().Dy())
@@ -53,7 +59,26 @@ func processCell(img image.Image, y int, x int, sz_x int, sz_y int) rune {
 		}
 	}
 	res := count / ((n - x) * (m - y))
-	return getChar(res)
+
+	clr := []int{0, 0, 0}
+	for i := x; i < n; i++ {
+		for j := y; j < m; j++ {
+			r, g, b := getColor(img.At(i, j))
+			clr[0] += r
+			clr[1] += g
+			clr[2] += b
+		}
+	}
+	mx := 0
+	mx_i := 0
+	for i := range clr {
+		if clr[i] > mx {
+			mx = clr[i]
+			mx_i = i
+		}
+	}
+
+	return getChar(res), mx_i
 }
 
 func minMax(a int, b int) (int, int) {
@@ -86,7 +111,7 @@ func getDeltas(img image.Image) (int, int) {
 	return sz_x, sz_y
 }
 
-func convertToAscii(img image.Image) [][]rune {
+func convertToAscii(img image.Image) ([][]rune, [][]int) {
 	sz_x, sz_y := img.Bounds().Dx(), img.Bounds().Dy()
 
 	delta_x, delta_y := getDeltas(img)
@@ -94,16 +119,19 @@ func convertToAscii(img image.Image) [][]rune {
 	sz_y_new, sz_x_new := sz_y/delta_y+1, sz_x/delta_x+1
 
 	textImg := make([][]rune, sz_y_new)
+	colorImg := make([][]int, sz_y_new)
+
 	for i := 0; i < sz_y_new; i++ {
 		textImg[i] = make([]rune, sz_x_new)
+		colorImg[i] = make([]int, sz_x_new)
 	}
 
 	for i := 0; i < sz_y; i += delta_y {
 		for j := 0; j < sz_x; j += delta_x {
-			textImg[i/delta_y][j/delta_x] = processCell(img, i, j, delta_x, delta_y)
+			textImg[i/delta_y][j/delta_x], colorImg[i/delta_y][j/delta_x] = processCell(img, i, j, delta_x, delta_y)
 		}
 	}
-	return textImg
+	return textImg, colorImg
 }
 
 func main() {
@@ -120,11 +148,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	textImg := convertToAscii(img_decoded)
+	textImg, colorImg := convertToAscii(img_decoded)
 
 	for i := range textImg {
 		for j := range textImg[i] {
-			fmt.Printf("%c", textImg[i][j])
+			fmt.Printf(colors[colorImg[i][j]], textImg[i][j])
 		}
 		fmt.Println()
 	}
